d49a04ccf294c5b1dcfdce82a7fd598ed7f7e0c9
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-02-18 00:15:07 -0800

    Feat: user-routes progress. Stopping point though

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,50 @@
+// Starting off the route-work with users!
+const router = require('express').Router();
+const { User, Post, Comment, } = require('../../models');
+
+// Find all users 
+router.get('/', (req, res) => {
+    User.findAll ({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+// Find single user
+router.get('/:id', (req, res) => {
+    User.findOne ({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_content', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }            
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
