955b8e608ac23d699ead7d9546aac64d46cc346b
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-02-17 23:26:10 -0800

    Feat: Added bcrypt hooks and wrapped up the rest of the users model

--- models/Users.js
+++ models/Users.js
@@ -33,4 +33,24 @@ User.init (
             }
         }
     },
-)
+    // Set up hooks that will allow bcrypt to hash newUserData passwords.
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
