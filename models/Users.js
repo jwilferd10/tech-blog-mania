54ee2c49a0c767af76bfd2ece4186a4ac8778263
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-02-17 23:24:06 -0800

    Feat: Added columns for id, username, email, password on user model

---
+++ models/Users.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends model {};
+
+// Define the id, email, password columns
+User.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+)
