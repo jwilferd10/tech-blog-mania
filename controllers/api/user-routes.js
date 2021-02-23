a3d25af2fa95b9a5efbcfb1b841ae2d5a62a2f63
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-02-22 22:34:26 -0800

    Feat: Login and Logout added to user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -48,3 +48,80 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+/* 
+Expects something like
+    {
+     username: 'ExampleName', 
+     email: 'example@gmail.com', 
+     password: 'password1234'
+    }
+*/
+router.post('/', (req, res) => {
+    User.create ({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    // Sessions setup, going to request user id, name and if logged in has been switched to true
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//***LOGIN AND LOGOUT START***//
+
+// This route will be found at http://localhost:3001/api/users/login in the browser.
+router.post('/login', (req, res) => {
+    User.findOne ({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+    
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        // Regular sessions setup, however notify when successfully logged in
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'Success, you are now logged in!' });
+        });
+    });
+});
+
+// If we gotta workout on a timeout, I bet something will have to be changed here
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+//***LOGIN AND LOGOUT END***//
+
