5f69da9ed1f712462ad14b5455fb22b2f11eb98a
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-03-22 22:18:26 -0700

    Feat: Editing a post with ID created

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -40,3 +40,49 @@ router.get('/', withAuth, (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne ({
+        where:{
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'created_at',
+            'title',
+            'post_content'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [ 'id', 'comment_text', 'post_id', 'user_id', 'created_at' ],
+                include: {
+                    model: User,
+                    attributes: [ 'username' ]
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'There is no post found with this id' });
+            return;
+        }
+
+        // Serialize
+        const post = dbPostData.get({ plain: true });
+
+        res.render('edit-post', {
+            post,
+            loggedIn: true
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
